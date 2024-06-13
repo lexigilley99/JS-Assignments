742f9ca1bea25938531c235916aea671fc82e9df
lexigilley99 <lexigilley99@gmail.com>
2024-06-13 13:56:59 -0400

    updated code for RPS part 1

---
+++ rPS1.js
@@ -0,0 +1,40 @@
+const hands = ['rock','paper','scissors'];
+
+function getHand() {
+    return hands[parseInt(Math.random()*10) % 3];
+}
+
+const player1 = {
+    name: 'Player 1',
+    getHand: getHand
+};
+
+const player2 = {
+    name: 'Player 2',
+    getHand: getHand
+};
+
+function playRound(player1, player2) {
+    const hand1 = player1.getHand();
+    const hand2 = player2.getHand();
+
+    console.log(`${player1.name} plays ${hand1}`);
+    console.log(`${player2.name} plays ${hand2}`);
+
+    if (hand1 === hand2) {
+        console.log("It's a tie!");
+        return null;
+    } else if (
+        (hand1 === 'rock' && hand2 === 'scissors') ||
+        (hand1 === 'scissors' && hand2 === 'paper') ||
+        (hand1 === 'paper' && hand2 === 'rock')
+    ) {
+        console.log(`${player1.name} wins!`);
+        return player1;
+    } else {
+        console.log(`${player2.name} wins!`);
+        return player2;
+    }
+}
+
+playRound(player1, player2);
