92feefc28dd693174fafd2925535a5b5d744c6fc
lexigilley99 <lexigilley99@gmail.com>
2024-06-14 15:14:30 -0400

    final RPS Part 2

--- rPS1.js
+++ rPS1.js
@@ -37,22 +37,25 @@ function playRound(player1, player2) {
     }
 }
 
-playRound(player1, player2);
-
-function playGame(player1, player2, playUntil) {
-    let scorePlayer1 = 0;
-    let scorePlayer2 = 0;
-
-    while (scorePlayer1 < playUntil && scorePlayer2 < playUntil) {
-
-        scorePlayer1++;
-    }
-    if (scorePlayer1 >= playUntil) {
-        return player1;
-    } else {
-        return player2;
+const playGame = (player1 ,player2 , playUntil) => {
+    let score1 = 0, score2 = 0, round = 1;
+    while (score1 < playUntil && score2 < playUntil) {
+        console.log(`--- Round ${round++} ---`);
+        const winner = playRound(player1, player2);
+        if (winner) {
+            winner === player1 ? score1++ : score2++;
+        }
+        console.log('--- Game Over ---');
+        if (score1 === playUntil) {
+            console.log(`${player1.name} wins the game!`);
+            return player1;
+        } else {
+            console.log(`${player2.name} wins the game!`);
+            return player2;
+        }
     }
 }
+
 
 function playTournament(player1, player2, player3, player4, playUntil) {
     const winner1 = playGame(player1, player2, playUntil);
@@ -62,10 +65,14 @@ function playTournament(player1, player2, player3, player4, playUntil) {
     console.log(`${tournamentWinner.name} is the world champion`);
 }
 
-const player1 = { name: "Player 1" };
-const player2 = { name: "Player 2" };
-const player3 = { name: "Player 3" };
-const player4 = { name: "Player 4" };
+const player3 = { 
+                name: "Player 3", 
+                getHand: getHand
+};
+const player4 = { 
+                name: "Player 4",
+                getHand: getHand
+};
 const playUntil = 3;
 
 playTournament(player1, player2, player3, player4, playUntil);
